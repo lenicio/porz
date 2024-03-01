41b9f0fa387858244e5f9ab8d136f69fbe8a45c6
Lenício Jr <lenicio.junior@hotmail.com>
2024-03-01 11:11:19 -0300

    Inclusão do campo select no HTML

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+
+Desenvolva um código que receba a idade e o sexo de uma pessoa, e informe se ela é obrigada a se 
+alistar no exército. 
+
+Regras de obrigatoriedade:
+- Homens com idade menor que 18 anos - Não obrigatório
+- Homens com idade entre 18 e 45 anos - Obrigatório
+- Mulheres - Não obrigatório
+*/
+
 // Essa função tem o objetivo de verificar se o array $_GET está vazio
 if(!empty($_GET['nome']) && !empty($_GET['idade'])) {
   $nome = $_GET['nome'];
@@ -21,6 +32,13 @@ if(!empty($_GET['nome']) && !empty($_GET['idade'])) {
   <form method="get">
     <input type="text" name="nome" placeholder="Informe seu nome..." required>
     <input type="number" name="idade" placeholder="Informe sua idade..." required>
+    <input type="date" name="data">
+
+    <select name="genero">
+      <option value="m">Masculino</option>
+      <option value="f">Feminino</option>
+    </select>
+    
     <button type="submit">Enviar</button>
   </form>
 
